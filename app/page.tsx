efcc88541de1490ee2f74e1e04a21c5605d9bd92
Lucas <lugab037@gmail.com>
2024-11-11 15:00:54 -0300

    First commit.

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,115 @@
-import Image from "next/image";
+"use client";
+
+import { useState } from "react";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [name, setName] = useState("");
+  const [partyCode, setPartyCode] = useState<number | "">("");
+  const [joinPartyClicked, setJoinPartyClicked] = useState(false);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  function createParty() {
+    if (name == "")
+      return;
+    
+    const partyData = { name };
+
+    alert(JSON.stringify(partyData));
+
+    fetch("http://localhost:8080/api/mafia/party/create", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(partyData),
+    })
+      .then((response) => {
+        if (response.ok) {
+          response.json()
+            .then((data) => {
+              window.location.href = "/game"
+            })
+            .catch((error) => {
+              console.log("Erro ao processar o corpo da resposta: " + error.message);
+            });
+        } else {
+          alert("Falha ao criar partida");
+        }
+      })
+      .catch((error) => {
+        console.error("Erro na requisição:", error);
+        alert("Erro na requisição: " + error.message);
+      });
+  }
+
+  return (
+    <div className="bg-gradient-to-r from-blue-500 to-purple-600">
+
+      {
+        joinPartyClicked?
+
+        <div className="h-screen w-screen flex flex-col justify-center items-center gap-6">
+          <h1 className="text-3xl sm:text-4xl font-extrabold text-white mb-6">
+            Cidade Dorme
+          </h1>
+
+          <input
+            type="text"
+            placeholder="Código da Partida"
+            onChange={(e) => {
+              const value = parseInt(e.target.value);
+              setPartyCode(isNaN(value) ? "" : value);
+            }}
+            
+            value={partyCode === "" ? "" : partyCode}
+            className="w-[90%] sm:w-[400px] h-[50px] px-4 text-[#333] rounded-lg shadow-lg focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-transparent transition-all duration-300"
+          />
+
+          <div className="w-screen flex flex-col sm:flex-row items-center justify-center gap-1">
+            <div onClick={createParty} className="w-[90%] sm:w-[300px] h-[50px] bg-blue-600 text-white rounded-lg flex justify-center items-center cursor-pointer transition-all duration-300 ease-in-out hover:bg-blue-700 active:scale-95">
+              Entrar
+            </div>
+
+            <div className="w-[90%] sm:w-[300px] h-[50px] bg-gray-600 text-white rounded-lg flex justify-center items-center cursor-pointer transition-all duration-300 ease-in-out hover:bg-gray-700 active:scale-95"
+            onClick={() => {
+              setJoinPartyClicked(false);
+            }}
+            >
+              Voltar
+            </div>
+          </div>
+
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+
+        :
+
+        <div className="h-screen w-screen flex flex-col justify-center items-center gap-6">
+          <h1 className="text-3xl sm:text-4xl font-extrabold text-white mb-6">
+            Cidade Dorme
+          </h1>
+
+          <input
+            type="text"
+            placeholder="Seu nome"
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+            className="w-[90%] sm:w-[400px] h-[50px] px-4 text-[#333] rounded-lg shadow-lg focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-transparent transition-all duration-300"
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+          <div className="w-screen flex flex-col sm:flex-row items-center justify-center gap-1">
+
+            <div className="w-[90%] sm:w-[300px] h-[50px] bg-blue-600 text-white rounded-lg flex justify-center items-center cursor-pointer transition-all duration-300 ease-in-out hover:bg-blue-700 active:scale-95"
+            onClick={() => {
+              if (name != "")
+                setJoinPartyClicked(true);
+              }}
+            >
+              Entrar
+            </div>
+
+            <div onClick={createParty} className="w-[90%] sm:w-[300px] h-[50px] bg-blue-600 text-white rounded-lg flex justify-center items-center cursor-pointer transition-all duration-300 ease-in-out hover:bg-blue-700 active:scale-95">
+              Criar Partida
+            </div>
+          </div>
+        </div>
+      }
     </div>
   );
 }
